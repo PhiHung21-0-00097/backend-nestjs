2f02ccbcda875ec1612e484a3d8f2f06a32b0e1e
Phihung21-0-00097 <0943244904hung@gmail.com>
2024-09-18 11:51:07 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-async function bootstrap() {
+import { Server } from 'http';
+import { Handler, Context, Callback } from 'aws-lambda';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({ origin: '*' });
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(3333);
+// }
+// bootstrap();
+let server: Server;
+
+const bootstrapServer = async () => {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3333);
-}
-bootstrap();
+  await app.init();
+  return app.getHttpAdapter().getInstance();
+};
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrapServer());
+  const response = await new Promise((resolve) => {
+    server.emit('request', event, resolve);
+  });
+  return response;
+};
